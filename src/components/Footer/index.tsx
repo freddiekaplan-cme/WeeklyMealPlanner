a3ba7d96ce116f47884190d5f3850b8597a68b61
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-09-20 14:58:37 +0200

    Add styling and Footer component.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<div className="bg-green-600 text-center">
+			<div className="py-10 mt-8 text-white">© 2023 Freddie Kaplan </div>
+		</div>
+	)
+}
+
+export default Footer
